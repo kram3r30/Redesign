a6b0ab2c932b7f6e697bcbd57685ff49846a096c
kram3r30 <alister.mark3r@gmail.com>
2023-03-02 21:33:26 -0500

    WIP header complete

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,55 @@
-// store the link plus the API key in a variable
+import { setWithExpiry, getWithExpiry } from "./modules/localStorageHelpers.js";
+
 const key = "pb3WbIQux4hi9ZGGD38jXNA1K5gjBt6j";
 const API = `https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=${key}`;
+const storagePrefix = "nyt-autosave";
+
+// function setWithExpiry(key, value, ttl) {
+//   const now = new Date();
+//   // `item` is an object which contains the original value
+//   // as well as the time when it's supposed to expire
+//   const item = {
+//     value: value,
+//     expiry: now.getTime() + ttl,
+//   };
+//   localStorage.setItem(key, JSON.stringify(item));
+// }
+
+// function getWithExpiry(key) {
+//   const itemStr = localStorage.getItem(key);
+//   // if the item doesn't exist, return null
+//   if (!itemStr) {
+//     console.log("no item string");
+//     return null;
+//   }
+//   console.log("item string found!", itemStr);
+//   const item = JSON.parse(itemStr);
+//   const now = new Date();
+//   // compare the expiry time of the item with the current time
+//   if (now.getTime() > item.expiry) {
+//     // If the item is expired, delete the item from storage
+//     // and return null
+//     localStorage.removeItem(key);
+//     return null;
+//   }
+//   return item.value;
+// }
 
 function getStories() {
-  fetch(API)
-    .then((response) => response.json())
-    .then((data) => showData(data.results));
+  const stories = getWithExpiry(storagePrefix);
+  if (!stories) {
+    console.warn(" stories expired - fetching again ");
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => showData(data.results));
+  } else {
+    console.warn(" stories not expired - no fetching ");
+    document.querySelector(".stories").innerHTML = stories;
+  }
 }
 
 function showData(stories) {
-  var looped = stories
+  const looped = stories
     .map(
       (story) => `
     <div class="item">
@@ -27,6 +67,7 @@ function showData(stories) {
     .join("");
 
   document.querySelector(".stories").innerHTML = looped;
+  setWithExpiry(storagePrefix, looped, 1000 * 60);
 }
 
 if (document.querySelector(".home")) {
