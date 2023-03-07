c58479c020cd82677eb96892fc2ca39593b10e9a
kram3r30 <alister.mark3r@gmail.com>
2023-03-07 16:26:29 -0500

    api key changed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,39 +1,8 @@
 import { setWithExpiry, getWithExpiry } from "./modules/localStorageHelpers.js";
 
-const key = "pb3WbIQux4hi9ZGGD38jXNA1K5gjBt6j";
-const API = `https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=${key}`;
+const key = "r6docL1BtytejUjST2TEPADAgG4DOEtu";
+const API = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${key}`;
 const storagePrefix = "nyt-autosave";
-
-// function setWithExpiry(key, value, ttl) {
-//   const now = new Date();
-//   // `item` is an object which contains the original value
-//   // as well as the time when it's supposed to expire
-//   const item = {
-//     value: value,
-//     expiry: now.getTime() + ttl,
-//   };
-//   localStorage.setItem(key, JSON.stringify(item));
-// }
-
-// function getWithExpiry(key) {
-//   const itemStr = localStorage.getItem(key);
-//   // if the item doesn't exist, return null
-//   if (!itemStr) {
-//     console.log("no item string");
-//     return null;
-//   }
-//   console.log("item string found!", itemStr);
-//   const item = JSON.parse(itemStr);
-//   const now = new Date();
-//   // compare the expiry time of the item with the current time
-//   if (now.getTime() > item.expiry) {
-//     // If the item is expired, delete the item from storage
-//     // and return null
-//     localStorage.removeItem(key);
-//     return null;
-//   }
-//   return item.value;
-// }
 
 function getStories() {
   const stories = getWithExpiry(storagePrefix);
